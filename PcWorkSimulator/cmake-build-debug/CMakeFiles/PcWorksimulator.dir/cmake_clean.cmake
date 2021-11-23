ff9a905d30e84ec5d6f3f2e6d5d2d915468982e8
supercat3900 <94914328+supercat3900@users.noreply.github.com>
2021-11-23 17:28:38 +0300

    PcWorkSimulator

---
+++ PcWorkSimulator/cmake-build-debug/CMakeFiles/PcWorksimulator.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PcWorkSimulator.dir/src/cpu.cpp.obj"
+  "CMakeFiles/PcWorkSimulator.dir/src/disk.cpp.obj"
+  "CMakeFiles/PcWorkSimulator.dir/src/gpu.cpp.obj"
+  "CMakeFiles/PcWorkSimulator.dir/src/kbd.cpp.obj"
+  "CMakeFiles/PcWorkSimulator.dir/src/main.cpp.obj"
+  "PcWorkSimulator.exe"
+  "PcWorkSimulator.exe.manifest"
+  "PcWorkSimulator.pdb"
+  "libPcWorkSimulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PcWorkSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PcWorkSimulator/include/disk.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void save(int *ram, int lenght);
+
+void load(int *ram, int lenght);
